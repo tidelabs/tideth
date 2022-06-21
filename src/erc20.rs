0a9f860121910845cdc00a1610f298a10372020e
Evan Feenstra <evanfeenstra@gmail.com>
2022-06-21 16:57:48 -0700

    balance_of accepts Address instead of str

--- src/erc20.rs
+++ src/erc20.rs
@@ -15,19 +15,16 @@
 // along with tideth.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::Result;
-use ethcontract::{prelude::*, transport::DynTransport};
-use std::str::FromStr;
+use ethcontract::{prelude::*, transport::DynTransport, Address};
 
 ethcontract::contract!("https://tidefi-contracts.s3.eu-west-1.amazonaws.com/Tether.json");
 
 pub async fn balance_of(
   web3: &Web3<DynTransport>,
-  asset_address: &str,
-  address: &str,
+  asset_address: Address,
+  address: Address,
 ) -> Result<u128> {
-  let asset_addy = H160::from_str(asset_address)?;
-  let tether = Tether::at(&web3, asset_addy);
-  let addy = H160::from_str(address)?;
-  let bal = tether.balance_of(addy).call().await?;
+  let tether = Tether::at(&web3, asset_address);
+  let bal = tether.balance_of(address).call().await?;
   Ok(bal.as_u128())
 }
